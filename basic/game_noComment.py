31da4c415af3ca381512a3a7c63e515d85dee057
twohlee <two_h_lee@naver.com>
2019-12-18 15:55:28 +0900

    3일차 수업 내용

---
+++ basic/game_noComment.py
@@ -0,0 +1,11 @@
+print("Enjoy Custom Game World")
+while True:
+    tmp = input('게임 제목을 입력하시오, 단 20자 이내로 입력 가능합니다. => ').strip()
+    if not tmp:
+        print('정확하게 입력하세요!')
+    elif len(tmp>20):
+        print("20자가 초과되었습니다.") 
+    else:
+        gameTitle = tmp
+        break
+print('gameTitle', gameTitle)
